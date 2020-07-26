c0852be0f9f0c242da7891c1fed476608b2caa4f
中山佳重 <nakayama_yoshie@manabito.biz>
2020-07-26 23:56:59 +0900

    Create pages template file for LP. #19

---
+++ page-lp.php
@@ -0,0 +1,42 @@
+<?php /*
+Template Name: landing page
+*/ ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <?php if ( !is_user_logged_in() ) : ?>
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-WT69J9');</script>
+        <!-- End Google Tag Manager -->
+        <script data-ad-client="ca-pub-1528327588378459" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <?php endif; ?>
+    <meta charset="<?php esc_attr( bloginfo( 'charset' ) ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="format-detection" content="telephone=no">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <?php if ( !is_user_logged_in() ) : ?>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WT69J9" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    <?php endif; ?>
+    <main class="section">
+        <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <section class="container">
+                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                    <div class="content">
+                        <?php the_title('<h1 class="title">', '</h1>');
+                        the_content(); ?>
+                    </div>
+                <?php endwhile; endif; ?>
+            </section>
+        </div>
+    </main>
+<?php wp_footer(); ?>
+</body>
+</html>
